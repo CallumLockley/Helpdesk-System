327e47b01b7393f8b6fcb72d1c147dfd83cd4006
Callum Lockley <lockleycallum1@gmail.com>
2022-02-08 22:02:50 +0000

    Reworked how login/auth is done.

--- src/helpdesk_system/app/routes/dashboard/settings/update_password.php
+++ src/helpdesk_system/app/routes/dashboard/settings/update_password.php
@@ -7,38 +7,50 @@ $app->post('/settings/update_password', function (Request $request, Response $re
 
     $tainted = $request->getParsedBody();
     $current = $tainted['current_password'];
-    $tainted_password_first = $tainted['password_first'];
-    $tainted_password_second = $tainted['password_second'];
+    $password_first = $tainted['password_first'];
+    $password_second = $tainted['password_second'];
+    $userId = $_SESSION['user_id'];
     $database = $app->getContainer()->get('database');
+    //Hash new Password
+    $hash = currentPassword($current,$database, $userId);
+    $verified_password = verifying($password_first,$password_second, $app, $hash);
 
-    checkPasswords($app, $current, $tainted_password_first, $tainted_password_second);
-
-
-
-
-//    return $response->withRedirect(URL_root . '/dashboard');
+//  if($verified_password)
+//  {
+//      if($database->updatePassword($userId, $verified_password))
+//      {
+//          return $response->withRedirect(URL_root . '/dashboard');
+//      }else{
+//          return $response->withRedirect(URL_root . '/settings');
+//      }
+//  }else{
+//       return $response->withRedirect(URL_root . '/settings');
+//  }
 });
 
-function checkPasswords($app, $current, $first_password, $second_password)
+function verifying($password_first, $password_second, $app, $current)
 {
-    $error = false;
-    var_dump(checkPasswordMatch($app, $current, $first_password, $second_password));
-//    if(strlen($first_password) >= 8)
-}
-
-function checkPasswordMatch($app, $current_password, $password_one, $password_two)
-{
-
-    $error = false;
-    if($password_one != $password_two)
+    if(twoNewMatch($password_first, $password_second))
     {
-        $error = true;
+        if(checkNewPassword($password_first))
+        {
+            $new_hash = hashPassword($app, $password_first);
+            if(compareNewOld($new_hash, $current))
+            {
+                //TODO
+                //Add updating of DB value
+                //Redirect to dashboard
+                //error message displayed if needed
+                return $new_hash;
+            }else{
+                return false;
+            }
+        }else{
+            return false;
+        }
+    }else{
+        return false;
     }
-    if($current_password == $password_one)
-    {
-        $error = true;
-    }
-    return $error;
 }
 
 function hashPassword($app, $password)
@@ -47,9 +59,39 @@ function hashPassword($app, $password)
     return $bcrypt->generateHash($password);
 }
 
-function updatePassword($app, $password)
+function compareNewOld($new_password, $old_password)
 {
+    if($new_password != $old_password)
+    {
+        return true;
+    }else{ return false; }
+}
 
-    $database = $app->getContainer()->get('database');
+function twoNewMatch($password_one, $password_two)
+{
+    if($password_one == $password_two)
+        return true;
+    else
+        return false;
+}
 
+function checkNewPassword($password)
+{
+    // Validate password strength
+    $upper = preg_match('@[A-Z]@', $password);
+    $lower = preg_match('@[a-z]@', $password);
+    $num    = preg_match('@[0-9]@', $password);
+    $special = preg_match('@[^\w]@', $password);
+
+    if(!$upper || !$lower || !$num || !$special || strlen($password) < 8) {
+        return false;
+    }else{
+        return true;
+    }
+}
+
+function currentPassword($current, $database,$userId){
+
+    $password = $database->getUserPassword($userId);
+    return $password;
 }
