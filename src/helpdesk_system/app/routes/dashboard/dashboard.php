1a26a8cc8d6f90498894ea9c7297e0f86dccf971
Callum Lockley <lockleycallum1@gmail.com>
2022-04-20 13:11:41 +0100

    Added check for user being logged in when accessing dashboard.php

--- src/helpdesk_system/app/routes/dashboard/dashboard.php
+++ src/helpdesk_system/app/routes/dashboard/dashboard.php
@@ -6,22 +6,19 @@ use Psr\Http\Message\ResponseInterface as Response;
 $app->GET('/dashboard',
     function(Request $request, Response $response) use ($app){
     $this->database->openConnection();
-    dashboard($app, $response);
 
+    if($_SESSION['username'] == NULL)
+    {
+        return $response->withRedirect(URL_root . '/');
+    }else{
+        $view = $app->getContainer()->get('view');
+        $view->render($response,
+            'dashboard_page.html.twig',[
+                'page_heading_1' => APP_NAME,
+                'css_path' => CSS_PATH,
+                'username' => $_SESSION['username'],
+                'permission' => $_SESSION['userPerms'],
+                'dashboard_route' => URL_root . '/dashboard',
+                'nav_image_path' => IMAGES_PATH.'helpdesk-header-image.png'
+            ]);    }
 })->setName('dashboard');
-
-
-function dashboard($app, $response) : void {
-
-    $view = $app->getContainer()->get('view');
-    $view->render($response,
-        'dashboard_page.html.twig',[
-            'page_heading_1' => APP_NAME,
-            'css_path' => CSS_PATH,
-            'username' => $_SESSION['username'],
-            'permission' => $_SESSION['userPerms'],
-            'dashboard_route' => URL_root . '/dashboard',
-            'nav_image_path' => IMAGES_PATH.'helpdesk-header-image.png'
-        ]);
-
-}
